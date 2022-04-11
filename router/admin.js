c617064507a6ada3c534c347f613db230d5114ca
tharshavardhanreddy <tatireddyharshavardhan@gmail.com>
2022-04-11 16:55:38 +0530

    router/admin.js -- tpe and module commented

--- router/admin.js
+++ router/admin.js
@@ -4,8 +4,10 @@ const {protect,authorize}=require('../middleware/user');
 const adminController=require('../controller/admin')
 
 adminRouter.get("/users",protect,adminController.getUsers)
-adminRouter.put('/firstApproval',protect,authorize({type:"WRITE",module:"User Management"}),adminController.firstApproval)
-adminRouter.put('/secondApproval',protect,authorize({type:"WRITE",module:"User Management"}),adminController.secondApproval)
-adminRouter.put("/",protect,authorize({type:"WRITE",module:"User Management"}),adminController.assignRoleToUser)
-
+// adminRouter.put('/firstApproval',protect,authorize({type:"WRITE",module:"User Management"}),adminController.firstApproval)
+// adminRouter.put('/secondApproval',protect,authorize({type:"WRITE",module:"User Management"}),adminController.secondApproval)
+// adminRouter.put("/",protect,authorize({type:"WRITE",module:"User Management"}),adminController.assignRoleToUser)
+adminRouter.put('/firstApproval',protect,adminController.firstApproval)
+adminRouter.put('/secondApproval',protect,adminController.secondApproval)
+adminRouter.put("/",protect,adminController.assignRoleToUser)
 module.exports=adminRouter
