fb24d7893491f8f6539dc340f592516ba9a5f8fd
tharshavardhanreddy <tatireddyharshavardhan@gmail.com>
2022-04-18 13:34:47 +0530

    changed auth in candi router

--- router/candidate.js
+++ router/candidate.js
@@ -5,7 +5,8 @@ const Candidate= require('../controller/candidate')
 
 const candidateRouter= express.Router();
 
-candidateRouter.post("/createCandidate",protect, authorize({permissionType:"WRITE",moduleName:"Candidate"}),Candidate.createCandidate);
+candidateRouter.post("/createCandidate",protect,Candidate.createCandidate);
+// authorize({permissionType:"WRITE",moduleName:"Candidate"}
 // requirementRouter.get("/listRequirement",protect, authorize({permissionType:"READ",moduleName:"Requirements"}),Requirement.listRequirement);
 // requirementRouter.post("/singleRequirement",protect, authorize({permissionType:"READ",moduleName:"Requirements"}),Requirement.SingleRequirementDetails);
 
