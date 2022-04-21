071ff642571b428fd26ed1a4169d20aac269028e
tharshavardhanreddy <tatireddyharshavardhan@gmail.com>
2022-04-21 15:05:15 +0530

    initial

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
 
 app.listen(port, () => {
-  log.info({ module: 'index' }, `Server started on port ${port}`)
+  log.info({ module: 'index' }, `Server is started on port ${port}`)
 })
 
 
