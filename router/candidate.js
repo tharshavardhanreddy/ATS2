e69f9131ba20064509c7318158b50612222e76e2
tharshavardhanreddy <tatireddyharshavardhan@gmail.com>
2022-04-25 15:13:18 +0530

    updated candidate js

--- router/candidate.js
+++ router/candidate.js
@@ -5,9 +5,9 @@ const Candidate= require('../controller/candidate')
 
 const candidateRouter= express.Router();
 
-candidateRouter.post("/createCandidate",protect,authorize({permissionType:"WRITE",moduleName:"Candidate"}),Candidate.createCandidate);
-// authorize({permissionType:"WRITE",moduleName:"Candidate"}
-candidateRouter.post("/assignedcandidates",protect, authorize({permissionType:"WRITE",moduleName:"Candidate"}),Candidate.assignedCandidates);
+candidateRouter.post("/createCandidate",protect,Candidate.createCandidate);
+// authorize({permissionType:"WRITE",moduleName:"Candidate"} authorize({permissionType:"WRITE",moduleName:"Candidate"})
+candidateRouter.post("/assignedcandidates",protect, Candidate.assignedCandidates);
 // requirementRouter.post("/singleRequirement",protect, authorize({permissionType:"READ",moduleName:"Requirements"}),Requirement.SingleRequirementDetails);
 
 
