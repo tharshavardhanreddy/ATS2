0ddad86be0a8bd8918a6b4c430da1dcda3c40750
tharshavardhanreddy <tatireddyharshavardhan@gmail.com>
2022-06-10 14:26:21 +0530

    cors() -- direct middleware

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-const corsOptions = {
-  origin: "*",
-  methods: ["POST", "GET", "PUT", "DELETE"]
-}
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["POST", "GET", "PUT", "DELETE"]
+// }
 
 //
 // const storage = multer.diskStorage({
@@ -53,22 +53,22 @@ const corsOptions = {
 // });
 //
 
-app.use(cors(corsOptions))
-app.use((req, res, next) => {
-  // console.log(req.hostname, req.headers, req.path);
+app.use(cors())
+// app.use((req, res, next) => {
+//   // console.log(req.hostname, req.headers, req.path);
 
-  try {
-    const allowedMethods = ["POST", "GET", "PUT", "DELETE"];
-    if (!allowedMethods.includes(req.method)) {
-      // errorResponse({ status: 400, result: `${req.method} method is not allowed`, res })
-      throw "not allowed"
+//   try {
+//     const allowedMethods = ["POST", "GET", "PUT", "DELETE"];
+//     if (!allowedMethods.includes(req.method)) {
+//       // errorResponse({ status: 400, result: `${req.method} method is not allowed`, res })
+//       throw "not allowed"
 
-    }
-  } catch (error) {
-    errorResponse({ status: 400, result: `${req.method} method is not allowed`, res })
-  }
-  next();
-});
+//     }
+//   } catch (error) {
+//     errorResponse({ status: 400, result: `${req.method} method is not allowed`, res })
+//   }
+//   next();
+// });
 
 
 //var upload = multer({ dest: "uploads/" });
